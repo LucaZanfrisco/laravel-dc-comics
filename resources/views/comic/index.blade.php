ccc344b4f49f5ef6872ef76becc335ee528ddfd7
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-11 16:33:26 +0200

    Add delete comic from db

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -24,6 +24,11 @@
                         <div>{{ $comic->writers }}</div> --}}
                         <a class="btn btn-success mt-3" href="{{ route('comics.show', $comic->id) }}">Detail</a>
                         <a href="{{ route('comics.edit', $comic->id)}}" class="btn btn-info my-3">Edit</a>
+                        <form action="{{ route('comics.destroy', $comic->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
                     </li>
                 @endforeach
             </ul>
